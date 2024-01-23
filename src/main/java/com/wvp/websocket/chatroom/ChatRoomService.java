0562d900ba81fd715739331fa439061059ea8b39
rafael <rafaelmarinhoalves5@gmail.com>
2024-01-22 22:08:45 -0300

    create ChatMessage

--- src/main/java/com/wvp/websocket/chatroom/ChatRoomService.java
+++ src/main/java/com/wvp/websocket/chatroom/ChatRoomService.java
@@ -11,17 +11,34 @@ public class ChatRoomService {
     private final ChatRoomRepository chatRoomRepository;
     public Optional<String> getChatRoomId(String senderId,
                                           String recipientId,
-                                          Boolean createNewRoomIfNotExists) {
+                                          Boolean createNewRoomIfNotExists)
 
-
+    {
         return chatRoomRepository.findBySenderIdAndRecipientId(senderId, recipientId).map(ChatRoom::getChatId)
-                .or(() ->){
+                .or(() ->{
             if (createNewRoomIfNotExists){
-                var
+                var chatId = createChatId(senderId, recipientId);
+                return Optional.of(chatId);
             }
             return Optional.empty();
-        }
+        });
+    }
 
+    private String createChatId(String senderId, String recipientId) {
+        var chatId =String.format("%s_%s", senderId, recipientId);
 
+        ChatRoom senderRecipient = ChatRoom.builder()
+                .chatId(chatId).
+                senderId(senderId).
+                recipientId(recipientId).
+                build();
+        ChatRoom recipientSender = ChatRoom.builder()
+                .chatId(chatId).
+                senderId(recipientId).
+                recipientId(senderId).
+                build();
+        chatRoomRepository.save(senderRecipient);
+        chatRoomRepository.save(recipientSender);
+        return chatId;
     }
 }
